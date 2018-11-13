739ad2170041442e44283fcca1e110c2ca9b2c4b
liammcdonald97 <liammcdonald97@gmail.com>
2018-11-12 23:07:52 -0500

    Add project files.

---
+++ step_162/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //example of const variable
+            const string schoolName = "The Tech Academy";
+            Console.WriteLine(schoolName);
+            //example of variable created using var keyword
+            var age = 23;
+            Console.WriteLine(age);
+            //example of new student object created byt just entering student name, and taking default Id value 0
+            Student student1 = new Student("Liam");
+            Console.WriteLine("{0} {1}", student1.Name, student1.ID);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ step_162/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_162
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public Student(string name) : this(name, 0)
+        {
+
+        }
+        public Student(string name, int Id)
+        {
+            Name = name;
+            ID = Id;
+        }
+    }
+}
